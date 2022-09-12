51ed3dad630f8a5ed60a5103284d7ba8c4632f1a
Kemal Kochekov <85355663+kemalkochekov@users.noreply.github.com>
2022-09-12 18:52:51 +0300

    Update Vocabulary Game Main.py

--- Vocabulary Game Main.py
+++ Vocabulary Game Main.py
@@ -13,6 +13,7 @@ pygame.mixer.music.play(loops=10)
 f=open("sample.txt","r")
 data=f.readlines()
 word_Count=len(data)
+#Creating new words list and answer list
 words_list=[]
 answer_list=[]
 for line in data:
@@ -29,7 +30,8 @@ for line in data:
             break
         every_word=every_word+letter
     answer_list.append(every_word[::-1])
-
+    
+#This dictionary collects words and answers
 my_dict=dict(zip(words_list,answer_list))
 
 #Screen details
@@ -42,11 +44,17 @@ y=(screen_height/2)-(app_height/2)
 root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 root.configure(bg="#ab9cf7")
 
+#Creating user's answer list which we can store any answer
 user_answer_list=["None"]*(word_Count+1)
-number=0                
+number=0
+
+#Welcome Page                
 label_word=Label(root,bg="#ab9cf7",text="WELCOME TO THE ENGLISH WORD QUIZ GAME",font=("Helvetica",32,"bold"))
 
+#Answer's checkbuttons
 checkbutton_list = []
+
+#Creating forward Button
 def forward(word_number):
     global label_word
     global next_btn
@@ -56,11 +64,12 @@ def forward(word_number):
     global user_answer_list
     global number
     number=word_number
-    #first page not showing
+    #forgetting 1 st page widgets
     label_word.place_forget()
     my_button1.place_forget()
     next_btn.place_forget()
     back_btn.place_forget()
+    
     return_menu_btn.place(relx=0,rely=0)
     label_word=Label(root,bg="#ab9cf7",text=words_list[word_number],font=("Helvetica",34,"bold"))
     next_btn=Button(root,text="NEXT",font=('Bold',20),bg='#1877f2',fg='white',width=12,command=lambda: forward(word_number+1))
@@ -75,7 +84,7 @@ def forward(word_number):
     user_answer_list[word_number]=Input.get()
     Input.delete(0,END)
 
-
+#Creating backword button
 def backward(word_number):
     global label_word
     global next_btn
@@ -83,6 +92,7 @@ def backward(word_number):
     global return_main_btn
     global number
     number=word_number
+    #forgetting last widgets
     label_word.place_forget()
     next_btn.place_forget()
     back_btn.place_forget()
@@ -101,12 +111,14 @@ def backward(word_number):
     user_answer_list[word_number]=Input.get()
     Input.delete(0,END)
     
-
+#Returning main menu page
 def return_main_menu():
     global label_word
     global next_btn
     global back_btn
     global Input
+    global result_btn
+    result_btn.place_forget()
     back_btn.place_forget()
     next_btn.place_forget()
     return_menu_btn.place_forget()
@@ -117,8 +129,12 @@ def return_main_menu():
     label_word=Label(root,bg="#ab9cf7",text="WELCOME TO THE ENGLISH WORD QUIZ GAME",font=("Helvetica",32,"bold"))
     label_word.place(relx=0.5, rely=0.4,anchor=CENTER)
     my_button1.place(relx=0.5, rely=0.5,anchor=CENTER)
+    for x in range(word_Count):
+        checkbutton_list[x].destroy()
+    checkbutton_list.clear()
 
 var=[]
+#Last page
 def result_page():
     global label_word
     global next_btn
@@ -145,7 +161,8 @@ def result_page():
         checkbutton_list[x].destroy()
     checkbutton_list.clear()
     var.clear()
-    
+
+#Creating finish button    
 def finish_page():
     global label_word
     global next_btn
@@ -176,17 +193,14 @@ def finish_page():
 Input=Entry(root,width=30,borderwidth=5,font=("Arial",28,"bold"))
 back_btn=Button(root,text="BACK",font=('Bold',20),bg='#1877f2',fg='white',width=12,state=DISABLED)
 next_btn=Button(root,text="NEXT",font=('Bold',20),bg='#1877f2',fg='white',width=12,command= lambda: forward(1))
+# next_btn.bind('<Return>',forward(1))
 return_menu_btn=Button(root,text="MENU",font=('Bold',20),bg='#1877f2',fg='white',width=12,command=return_main_menu)
 finish_btn=Button(root,text="FINISH",font=('Bold',20),bg='#1877f2',fg='white',width=12,command=finish_page)
 correct_button=Checkbutton(root,text="CHeck this box",variable=var,onvalue=1,offvalue=0)
 result_btn=Button(root,font=('Bold',20),bg='red',fg='black',text="Result",width=12,command=result_page)
-# label_word.place(relx=0.5,rely=0.45,anchor=CENTER) 
-# back_btn.place(relx=0.3,rely=0.94)
-# next_btn.place(relx=0.5,rely=0.94)     
-
 
     
-#First Page 
+#Game's 1st Page 
 
 my_button1=Button(root,text="START",padx=250,pady=15,bg="#f7ff59",activebackground="yellow",command=lambda: forward(0))
 label_word.place(relx=0.5, rely=0.4,anchor=CENTER)
